930c45c66da8deec43b2a14cb1cd9c593557b2c8
Artem Biryukov <carbon0093@gmail.com>
2022-07-08 11:47:43 +0300

    блок price, выбор дополнительных услуг

--- src/js/modules/pricesForm.js
+++ src/js/modules/pricesForm.js
@@ -1,51 +1,53 @@
 export const priceFrom = () => {
   // console.log('price select');
   const price = 250;
-  const priceItems = document.querySelectorAll('.price__item'),
-    popUpInput = document.querySelector('.popUp__input'),
-    popUpBtn = document.querySelector('.popUp__btn');
+  const priceItems = document.querySelectorAll('.price__item');
 
-  const popUpSelect = (target) => {
-    popUpBtn.addEventListener('click', (event) => {
-      let value = popUpInput.value;
+  const popUpSelect = (item) => {
+    let itemSrc = item.dataset.src;
+    const popUps = document.querySelectorAll('.popUp');
 
-      if (event.target.closest('.popUp__btn')) {
-        if (value > 0) {
-          target.classList.add('active');
-          target.setAttribute('data-select', '1');
+    popUps.forEach((popUp) => {
+      //Если ссылка на модалку равна id модалки
+      if (itemSrc === `#${popUp.id}`) {
+        let popUpBtn = popUp.querySelector('.popUp__btn');
 
-          target.querySelector('.additionals__count').innerText = value * price;
+        popUpBtn.addEventListener('click', (e) => {
+          let popUpInputValue = popUp.querySelector('.popUp__input').value;
 
-          //Отчистка инпута после закрытия окна
-          popUpInput.value = '';
-        } else if (value == 0 || value == '') {
-          target.classList.remove('active');
-          target.removeAttribute('data-select');
-          target.querySelector('.additionals__count').innerText = '1 шт. / 250';
-        }
+          if (popUpInputValue > 0) {
+            item.classList.add('active');
+            item.setAttribute('data-select', '1');
+            item.querySelector('.additionals__count').innerText = popUpInputValue * price;
+            //Отчистка инпута после закрытия окна
+            popUpInputValue = '';
+          } else if (popUpInputValue == 0 || popUpInputValue == '') {
+            item.classList.remove('active');
+            item.removeAttribute('data-select');
+            item.querySelector('.additionals__count').innerText = '1 шт. / 250';
+          }
+        });
       }
     });
   };
 
   priceItems.forEach((priceItem) => {
-    const additionalList = priceItem.querySelectorAll('.additionals__item');
+    const additionalList = priceItem.querySelector('.additionals__list');
 
-    additionalList.forEach((el) => {
-      el.addEventListener('click', (e) => {
-        const target = e.target;
+    additionalList.addEventListener('click', (e) => {
+      const target = e.target;
+      let item = target.closest('.additionals__item');
 
-        if (target.closest('.active')) {
-          target.classList.remove('active');
-          target.removeAttribute('data-select');
-        } else {
-          if (target.dataset.additionalName === 'retouch' || target.closest('span')) {
-            popUpSelect(target.closest('.additionals__item'));
-          } else {
-            target.classList.add('active');
-            target.setAttribute('data-select', '1');
-          }
+      if (item.classList.contains('active')) {
+        item.classList.remove('active');
+        item.removeAttribute('data-select');
+      } else {
+        if (item.dataset.additionalName === 'retouch') {
+          popUpSelect(item);
         }
-      });
+        item.classList.add('active');
+        item.setAttribute('data-select', '1');
+      }
     });
   });
 };
